b286dcde41bafbd07d7f770e720f981c2545557b
Vinícius Pereira de Oliveira <vipoliveira2012@live.com>
2022-03-31 20:24:33 -0300

    Finished

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+import 'package:cara_ou_coroa/ui/jogar_page.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: JogarPage(),
+    ),
+  );
+}
